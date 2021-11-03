89c7ad4117c7977b044ad2fa3c1c91b2e78533fa
Jade Neff <njade79162@gmail.com>
2021-11-02 21:17:51 -0400

    Went back to add finishing touches to second project

--- Challenge2_UnitTests/ClaimRepositoryTests.cs
+++ Challenge2_UnitTests/ClaimRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using Challenge2_ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Challenge2_UnitTests
 {
@@ -28,6 +29,43 @@ namespace Challenge2_UnitTests
             Claim actual = claimRepo.GetClaim(1);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_GetNextClaim()
+        {
+            ClaimRepository claimRepo = new ClaimRepository();
+            Claim expected = new Claim(1, "Description", ClaimType.Car, 20.00, new DateTime(2020, 7, 3), new DateTime(2020, 7, 5));
+            claimRepo.AddClaim(expected);
+
+            Claim actual = claimRepo.GetNextClaim();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_GetClaimDirectory()
+        {
+            ClaimRepository claimRepo = new ClaimRepository();
+            Claim claim = new Claim(1, "Description", ClaimType.Car, 20.00, new DateTime(2020, 7, 3), new DateTime(2020, 7, 5));
+            claimRepo.AddClaim(claim);
+            Queue<Claim> expected = new Queue<Claim>();
+            expected.Enqueue(claim);
+            Queue<Claim> actual = claimRepo.GetClaimDirectory();
+
+            bool areEqual = false;
+            foreach(Claim expectedClaim in expected)
+            {
+                if (expectedClaim != actual.Dequeue())
+                {
+                    areEqual = false;
+                    break;
+                }
+
+                areEqual = true;
+            }
+
+            Assert.IsTrue(areEqual);
         }
 
         [TestMethod]
